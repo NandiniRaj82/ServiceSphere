cbcc352192816e09b42bc940c1c7acd0e6dcf47e
Nandini Raj <nandiniraj175@gmail.com>
2025-01-24 09:39:46 +0530

    Update db.js

--- servicesphere_backend/config/db.js
+++ servicesphere_backend/config/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://nandiniraj175:service@service.daonv.mongodb.net/?retryWrites=true&w=majority&appName=service/service");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Database Connected");
     } catch (error) {
         console.error("Database connection error:", error);
-        process.exit(1); 
+        process.exit(1);
     }
 };
 
